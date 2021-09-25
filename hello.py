5e4d9c6ad7fb61e72cc88497e5f9a044654cbd78
Rahul Banerjee <rahul2699@yahoo.com>
2021-09-25 17:00:59 -0400

    Activity 3 - Added template to display navbar,user name and local time

--- hello.py
+++ hello.py
@@ -1,14 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,render_template
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+from datetime import datetime
 
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' %name
+    return render_template('user.html',name = name,current_time = datetime.utcnow())
 
 if __name__ == "__main__":
     app.run(debug=True)
