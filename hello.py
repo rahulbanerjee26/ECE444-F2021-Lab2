877a75c36cae2646ee9689f7c802854225273e3d
Rahul Banerjee <rahul2699@yahoo.com>
2021-09-26 17:14:20 -0400

    Activity4 - Added field for utoronto email check and relevant flash/header messages

--- hello.py
+++ hello.py
@@ -3,17 +3,24 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'random number'
+app.config['SECRET_KEY'] = 'super_secret_key'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class validateEmail(object):
+    def __call__(self, form, field):
+        email_id = field.data
+        if '@' not in email_id:
+            raise ValidationError(f'Please include an @ in the email. {field.data} is missing an @')
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UOFT email address?',validators=[DataRequired(),validateEmail()])
     submit = SubmitField('Submit')
 
 
@@ -26,11 +33,15 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),email=session.get('email'))
 
 
 if __name__ == "__main__":
